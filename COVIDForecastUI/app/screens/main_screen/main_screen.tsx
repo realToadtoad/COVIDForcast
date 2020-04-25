2100ee8a5fc2f21743fbe336694a9a6690985334
Arvind Vivekanandan <arvind.lab03@gmail.com>
2020-04-25 14:41:02 -0700

    modified map size

--- COVIDForecastUI/app/screens/main_screen/main_screen.tsx
+++ COVIDForecastUI/app/screens/main_screen/main_screen.tsx
@@ -129,7 +129,7 @@ const styles = StyleSheet.create({
   },
   map: {
     width: deviceWidth-25,
-    height: deviceHeight/2,
+    height: deviceHeight*0.75,
   },
   mapContainer: {
     justifyContent: "center",
