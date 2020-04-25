a1af6f5add0ba1d63b653e828e26ad0e0fe3c6ff
realToadtoad <kingdomkeepersguy@gmail.com>
2020-04-25 11:52:41 -0700

    added emojis

--- COVIDForecastUI/app/components/seven_day_forecast/seven_day_forecast.tsx
+++ COVIDForecastUI/app/components/seven_day_forecast/seven_day_forecast.tsx
@@ -14,6 +14,7 @@ import {
   Text,
 } from "@ui-kitten/components";
 import moment from "moment";
+import { currentCases } from "../current_cases/current_cases";
 
 let deviceHeight = Dimensions.get("window").height;
 let deviceWidth = Dimensions.get("window").width;
@@ -44,59 +45,93 @@ const sampleData = [
   {
     index: 2,
     date: "2020-04-27",
-    prediction: 42949
+    prediction: 42949,
   },
   {
     index: 3,
     date: "2020-04-28",
-    prediction: 47284
+    prediction: 47284,
   },
   {
     index: 4,
     date: "2020-04-29",
-    prediction: 51392
+    prediction: 51392,
   },
   {
     index: 5,
     date: "2020-04-30",
-    prediction: 56395
+    prediction: 56395,
   },
   {
     index: 6,
     date: "2020-05-01",
-    prediction: 58630
-  }
+    prediction: 99999,
+  },
 ];
 
 function getStyle(item: any) {
-  if(item.index % 2 == 0) {
-    return {backgroundColor: "#1c1c1c", borderWidth: 3, borderColor: "#191919"};
+  if (item.index % 2 == 0) {
+    return {
+      backgroundColor: "#1c1c1c",
+      borderWidth: 3,
+      borderColor: "#191919",
+    };
   } else {
-    return {backgroundColor: "#292929", elevation: 8};
+    return { backgroundColor: "#292929", elevation: 8 };
   }
 }
 
 function getCurrentStatusEmoji(item: any) {
- return "😃"; //ignore that happy for now
+  let prediction = item.prediction;
+  let yesterday;
+  if (item.index - 1 == -1) {
+    yesterday = currentCases;
+  } else {
+    yesterday = sampleData[item.index - 1].prediction;
+  }
+  if (prediction - yesterday < 0) {
+    return "🤔";
+  } else if (prediction - yesterday == 0) {
+    return "😃";
+  } else if (prediction - yesterday <= 100) {
+    return "🙂";
+  } else if (prediction - yesterday <= 1000) {
+    return "😐";
+  } else if (prediction - yesterday <= 9000) {
+    return "😟";
+  } else {
+    return "😫";
+  }
 }
 
 export class SevenDayForecast extends React.Component {
   render() {
     return (
       <Layout style={styles.container}>
-        <Text category="h6" style={{marginLeft: 12.5, fontFamily: "Metropolis-Regular", letterSpacing: 2.5}}>
+        <Text
+          category="h6"
+          style={{
+            marginLeft: 12.5,
+            fontFamily: "Metropolis-Regular",
+            letterSpacing: 2.5,
+          }}
+        >
           7 DAY FORECAST
         </Text>
-        <ScrollView horizontal={true} style={{width: deviceWidth, height: deviceHeight/6}}>
-          {sampleData.map(item => (
+        <ScrollView
+          horizontal={true}
+          style={{ width: deviceWidth, height: deviceHeight / 6 }}
+        >
+          {sampleData.map((item) => (
             <Layout style={[styles.forecastBox, getStyle(item)]}>
-              <Text category="label" style={{fontFamily: "Metropolis-Medium"}}>
+              <Text
+                category="label"
+                style={{ fontFamily: "Metropolis-Medium" }}
+              >
                 {moment(item.date).format("ddd").toUpperCase()}
               </Text>
-              <Text category="h3">
-                {getCurrentStatusEmoji(item)}
-              </Text>
-              <Text style={{fontFamily: "Metropolis-Bold"}}>
+              <Text category="h3">{getCurrentStatusEmoji(item)}</Text>
+              <Text style={{ fontFamily: "Metropolis-Bold" }}>
                 {item.prediction}
               </Text>
             </Layout>
@@ -115,12 +150,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   forecastBox: {
-    width: deviceWidth/5,
-    height: deviceHeight/6,
+    width: deviceWidth / 5,
+    height: deviceHeight / 6,
     marginVertical: 10,
     marginHorizontal: 6,
     alignItems: "center",
     justifyContent: "space-evenly",
-    borderRadius: 10
-  }
+    borderRadius: 10,
+  },
 });
