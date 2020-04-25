efc73c65a3e50c81a69f77f28227a82ebae62573
Arvind Vivekanandan <arvind.lab03@gmail.com>
2020-04-25 14:18:33 -0700

    map initial

--- COVIDForecastUI/app/screens/main_screen/main_screen.tsx
+++ COVIDForecastUI/app/screens/main_screen/main_screen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Dimensions, StatusBar } from "react-native";
+import { StyleSheet, View, Dimensions, StatusBar, ScrollView } from "react-native";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
 import {
   createAppContainer,
@@ -16,31 +16,59 @@ import {
 } from "@ui-kitten/components";
 
 import { CurrentCases } from "../../components/current_cases/current_cases";
-import { currentLocation, currentState } from "../../../App";
+import { currentLocation, currentState, currentLat, currentLong } from "../../../App";
 import { UnsplashWidget } from "../../components/unsplash_widget/unsplash_widget";
 
-let deviceHeight = Dimensions.get("window").height;
-let deviceWidth = Dimensions.get("window").width;
+import mapStyleJSON from '../../../mapStyle.json';
+
+import MapView,
+{ Marker, Callout, Polygon, Circle }
+  from 'react-native-maps';
+  
+let currLat = currentLat;
+let currLong = currentLong;
 
 export class MainScreen extends React.Component {
+  
   render() {
     return (
       <Layout style={styles.container}>
-        <StatusBar />
-        <UnsplashWidget />
-        <CurrentCases />
-        <View
-          style={{
-            flexDirection: "row",
-            marginTop: 5,
-            marginLeft: 12.5,
-            alignItems: "center",
-            alignSelf: "flex-start"
-          }}
-        >
-          <Icon style={styles.icon} name="pin-outline" />
-          <Text style={{ letterSpacing: 2 }}>{currentState.toUpperCase()}</Text>
-        </View>
+        <ScrollView>
+          <StatusBar />
+          <UnsplashWidget />
+          <CurrentCases />
+          <View
+            style={{
+              flexDirection: "row",
+              marginTop: 5,
+              marginLeft: 12.5,
+              alignItems: "center",
+              alignSelf: "flex-start"
+            }}
+          >
+            <Icon style={styles.icon} name="pin-outline" />
+            <Text style={{ letterSpacing: 2 }}>{currentState.toUpperCase()}</Text>
+          </View>
+          <View style={styles.mapContainer}>
+            <MapView style={styles.map} initialRegion={{
+              latitude: currLat,
+              longitude: currLong,
+              latitudeDelta: 15,
+              longitudeDelta: 15,
+            }}
+            customMapStyle={mapStyleJSON.mapStyle}
+            >
+              <Marker
+              coordinate={{ latitude: currLat, longitude: currLong }}>
+
+              <Callout>
+                <Text style={styles.calloutText}>Your Location</Text>
+              </Callout>
+
+            </Marker>
+            </MapView>
+          </View>
+        </ScrollView>
       </Layout>
     );
   }
@@ -55,4 +83,15 @@ const styles = StyleSheet.create({
     height: 20,
     width: 20,
   },
+  map: {
+    width: Dimensions.get('window').width-25,
+    height: Dimensions.get('window').height/2,
+  },
+  mapContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  calloutText: {
+    color: 'black'
+  }
 });
