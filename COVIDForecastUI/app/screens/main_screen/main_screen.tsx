56182361556092376c2b126a0d105772e72b328e
Arvind Vivekanandan <arvind.lab03@gmail.com>
2020-04-25 21:46:50 -0700

    maps radius visualization feature

--- COVIDForecastUI/app/screens/main_screen/main_screen.tsx
+++ COVIDForecastUI/app/screens/main_screen/main_screen.tsx
@@ -30,13 +30,24 @@ import { SevenDayForecast } from "../../components/seven_day_forecast/seven_day_
 
 import mapStyleJSON from '../../../mapStyle.json';
 import capitalsJSON from '../../../us-capitals.json';
+import capitalsJSON2 from '../../../us-capitals2.json';
 
 import MapView,
-{ Marker, Callout, Polygon, Circle }
+{ Marker, Callout, Polygon, Circle, Heatmap }
   from 'react-native-maps';
+
+import Papa from "papaparse";
+import moment from "moment";
 
 let deviceWidth = Dimensions.get('window').width;
 let deviceHeight = Dimensions.get('window').height;
+
+let states = ["Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", 
+"Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",
+   "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina",
+   "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+   "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "West Virginia", "Wisconsin",  "Wyoming"];
+let statesCases = new Array(50);
 
 function wait(timeout: number) {
   return new Promise((resolve) => {
@@ -47,9 +58,31 @@ function wait(timeout: number) {
 const blueMarker = require("../../../assets/blue-marker.png");
 const redMarker = require("../../../assets/red-marker.png");
 
-export class MainScreen extends React.Component {
+async function fetchCases(date: string, state: string, index: number) {
+  let now = moment(date).subtract(1, "days");
+  let dayStr = now.format("MM-DD-YYYY");
+  let url =
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/" +
+    dayStr +
+    ".csv";
+  let currentCases: number;
+  await Papa.parse(url, {
+    download: true,
+    complete: function (res) {
+      currentCases = res.data.find(function (obj: any) {
+        return obj[0] == state; // [0] is the column for the state
+      })[5];
+      statesCases[index] = currentCases;
+    },
+  });
+}
 
+export class MainScreen extends React.Component {
   async UNSAFE_componentWillMount() {
+    let now = moment();
+    for(var i = 0; i < 48; i++) {
+      await fetchCases(now.format("YYYY-MM-DD"), states[i], i);
+    }
     await wait(3000);
     this.forceUpdate();
   }
@@ -99,13 +132,13 @@ export class MainScreen extends React.Component {
             customMapStyle={mapStyleJSON.mapStyle}>
               <Marker image = {redMarker}
               coordinate={{ latitude: currentLocation.coords.latitude, longitude: currentLocation.coords.longitude }}>
-              <Callout>
+              <Callout tooltip>
                 <View style={styles.calloutContent}>
-                   <View style={styles.bubble}>
-                    <Text style={styles.calloutText}>Your Location</Text>
+                   <View style={styles.bubble2}>
+                    <Text style={styles.calloutHeader}>Your Location</Text>
                   </View>
-                  <View style={styles.arrowBorder} />
-                  <View style={styles.arrow} />
+                  <View style={styles.arrowBorder2} />
+                  <View style={styles.arrow2} />
                 </View>
               </Callout>
               </Marker>
@@ -114,15 +147,22 @@ export class MainScreen extends React.Component {
                 coordinate={{ latitude: parseFloat(item.lat), longitude:  parseFloat(item.long)}}>
                   <Callout tooltip>
                     <View style={styles.calloutContent}>
-                      <View style={styles.bubble}>
-                        <Text style={styles.calloutText}>{item.capital}, {item.name}</Text>
+                      <View style={styles.bubble1}>
+                        <Text style={styles.calloutHeader}>{item.name}</Text>
+                        <Text style={styles.calloutText}>Cases: {statesCases[(states.indexOf(item.name))]}</Text>
                       </View>
-                      <View style={styles.arrowBorder} />
-                      <View style={styles.arrow} />
+                      <View style={styles.arrowBorder1} />
+                      <View style={styles.arrow1} />
                     </View>
                   </Callout>
-
                 </Marker>
+              ))}
+              {jsonVals.map((item) => (
+                <Circle
+                  center={{ latitude: parseFloat(item.lat), longitude:  parseFloat(item.long) }}
+                  radius={item.cases * (Math.floor(Math.random() * 2) + 0.5)}
+                  fillColor={'rgba(252, 36, 3, 0.25)'}
+                />
               ))}
             </MapView>
           </View>
@@ -172,10 +212,16 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     overflow: "hidden"
   },
-  calloutText: {
-    color: 'black'
+  calloutHeader: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 17,
   },
-  bubble: {
+  calloutText: {
+    color: 'black',
+    fontSize: 16,
+  },
+  bubble1: {
     backgroundColor: "#4da2ab",
     padding: 10,
     borderRadius: 20,
@@ -183,7 +229,15 @@ const styles = StyleSheet.create({
     alignItems: `center`,
     justifyContent: `center`,
   },
-  arrow: {
+  bubble2: {
+    backgroundColor: "#eb4034",
+    padding: 10,
+    borderRadius: 20,
+    display: `flex`,
+    alignItems: `center`,
+    justifyContent: `center`,
+  },
+  arrow1: {
     backgroundColor: 'transparent',
     borderWidth: 16,
     borderColor: 'transparent',
@@ -191,11 +245,27 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     marginTop: -32,
   },
-  arrowBorder: {
+  arrowBorder1: {
     backgroundColor: 'transparent',
     borderWidth: 16,
     borderColor: 'transparent',
     borderTopColor: '#007a87',
+    alignSelf: 'center',
+    marginTop: -0.5,
+  },
+  arrow2: {
+    backgroundColor: 'transparent',
+    borderWidth: 16,
+    borderColor: 'transparent',
+    borderTopColor: '#eb4034',
+    alignSelf: 'center',
+    marginTop: -32,
+  },
+  arrowBorder2: {
+    backgroundColor: 'transparent',
+    borderWidth: 16,
+    borderColor: 'transparent',
+    borderTopColor: '#eb4034',
     alignSelf: 'center',
     marginTop: -0.5,
   },
